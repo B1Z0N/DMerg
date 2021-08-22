1ee24de92765cb871d197bf03983ece7e45e68d5
B1Z0N <kolausf@gmail.com>
2021-08-22 13:40:50 +0300

    Provided README.md.

--- use/main.py
+++ use/main.py
@@ -1,18 +1,18 @@
-from typing import Iterable, Mapping
+from typing import Iterable
 from dmerg import DataProcess, resolve_conflicts
 
-
-class CompareCurvesDataProcess(DataProcess):
+class CompositeCurvesDataProcess(DataProcess):
+	# override this one
     def _merge(self, key: str, src: Iterable, dest: Iterable):
+    	# just reshape it to needed format
         src = [{
-            'CurveId': el['Curve'],
+            'CurveId': el['Curve'], # keys(e.g. 'Curve') are headings of corresponding .csv file
             'Currency': el['Ccy'],
             'NumeratorCurves': el['Numerator'].split(','),
             'DenominatorCurves': [''] if el['Denominator'] == '' else el['Denominator'].split(','),
         } for el in src]
 
-        return resolve_conflicts(key, src, dest)
-
+        return resolve_conflicts(key, src, dest) # just resolve conflicts in manual mode
 
 if __name__ == '__main__':
-    print(DataProcess.run(CompareCurvesDataProcess()))
+    print(DataProcess.run(CompositeCurvesDataProcess())) # run on all data
